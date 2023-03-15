9f2aaf62c111a91a7ca3e5e458ea3d3413d41439
jeon-seongjae <tjdwoek62@gmail.com>
2023-03-15 15:38:06 +0900

    github action test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World! - Ver 0.2');
     });
   });
 });
